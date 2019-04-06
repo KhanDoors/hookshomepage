5c588b700a73cd59ae96d080c1787b6675248c40
unknown <syyadkhan@yahoo.com>
2019-04-06 18:14:23 -0400

    awesome so far

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>hello world</h1>
+const App = () => {
+  const [userQuery, setUserQuery] = useState("");
+
+  const updateUserQuery = e => {
+    console.log("userQuery", userQuery);
+    setUserQuery(e.target.value);
+  };
+
+  const handleKeyPress = e => {
+    if (e.key === "Enter") {
+      searchQuery();
+    }
+  };
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, "_blank");
+  };
+
+  return (
+    <div>
+      <h1>Syyad the Khandoor</h1>
+      <div className="form">
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={searchQuery}>Search</button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
