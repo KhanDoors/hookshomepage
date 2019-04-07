5167bd7d8112fed3529ce58a1a0e2dd345b74f45
unknown <syyadkhan@yahoo.com>
2019-04-06 21:12:42 -0400

    well its a bit much I  think

--- src/Stories.js
+++ src/Stories.js
@@ -13,17 +13,19 @@ const Stories = () => {
 
   return (
     <div className="Stories">
-      <h2>Stories</h2>
+      <h1>Stories</h1>
       {stories.map(story => {
         const { id, by, time, title, url } = story;
 
         return (
           <div key={id}>
             <a href={url} target="_blank" rel="noopener noreferrer">
-              {title}
+              <h2>{title}</h2>
             </a>
             <div>
-              {by} - {new Date(time * 1000).toLocaleString()}
+              <h3>
+                {by} - {new Date(time * 1000).toLocaleString()}
+              </h3>
             </div>
           </div>
         );
